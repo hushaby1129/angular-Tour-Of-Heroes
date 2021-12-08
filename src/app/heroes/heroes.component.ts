86940f193c4ec334880d0e5b2582507f33b6b76a
hushaby1129 <36461935+hushaby1129@users.noreply.github.com>
2021-12-08 18:31:51 +0900

    // 2021-12-08 2. Display a List complete

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 
 import { Hero } from "../hero"
+import { HEROES } from "../mock-heroes";
 
 @Component({
     selector: 'app-heroes',
@@ -9,10 +10,9 @@ import { Hero } from "../hero"
 })
 export class HeroesComponent implements OnInit {
 
-    hero: Hero = {
-        id: 1
-        , name: 'Windstorm'
-    }
+    selectedHero?: Hero;
+
+    heroes = HEROES;
 
     constructor() {
     }
@@ -23,6 +23,10 @@ export class HeroesComponent implements OnInit {
 
     ngOnChanges(): void {
         console.log("execute ngOnChanges")
+    }
+
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
     }
 
 }
