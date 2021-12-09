223c4171a21f53581630f22c9f8ef465ab25716b
hushaby1129 <36461935+hushaby1129@users.noreply.github.com>
2021-12-09 12:03:57 +0900

    // 2021-12-08 3. create a Feature Component 4. Add Services complete

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 
 import { Hero } from "../hero"
 import { HEROES } from "../mock-heroes";
+import { HeroService } from "../hero.service";
+import { MessageService } from "../message.service";
 
 @Component({
     selector: 'app-heroes',
@@ -12,21 +14,30 @@ export class HeroesComponent implements OnInit {
 
     selectedHero?: Hero;
 
-    heroes = HEROES;
+    // heroes = HEROES;
+    heroes: Hero[] = [];
 
-    constructor() {
+    constructor(
+        private heroService: HeroService
+        , private messageService: MessageService
+    ) {
     }
 
     ngOnInit(): void {
-        console.log("execute ngOnInit")
+        console.log("execute ngOnInit");
+        this.getHeroes();
     }
 
     ngOnChanges(): void {
-        console.log("execute ngOnChanges")
+        console.log("execute ngOnChanges");
     }
 
     onSelect(hero: Hero): void {
         this.selectedHero = hero;
+        this.messageService.add("HeroesComponent: Selected hero id=" + hero.id);
     }
 
+    getHeroes(): void {
+        this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+    }
 }
